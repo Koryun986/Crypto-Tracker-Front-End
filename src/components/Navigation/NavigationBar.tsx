c32b93dfcab485228b5c12a12c9d02879f1e046c
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-05-13 21:50:34 +0400

    add sass and base app structure

---
+++ src/components/Navigation/NavigationBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./../../styles/_navigationBar.scss";
+
+export default function NavigationBar() {
+    
+    return  (
+        <nav className="NavigationBar">
+            <div className="NavigationBar_Container">
+                <div className="NavigationBar_Logo">Crypto Tracker</div>
+            </div>
+        </nav>
+            );
+}
